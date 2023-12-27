0e60af570e1a46e24779f4b6ebd109649716771d
Arjun Sah <arjunsah@sussol.net>
2023-12-27 17:16:43 +0545

    create blog with valid token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,17 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization');
+	if (authorization && authorization.startsWith('Bearer ')) {
+		return authorization.replace('Bearer ', '');
+	}
+
+	return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -9,8 +19,13 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (req, res, next) => {
-	const { title, url, likes, author, userId } = req.body;
-	const user = await User.findById(userId);
+	const { title, url, likes, author } = req.body;
+	const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+	if (!decodedToken.id) {
+		return res.status(401).json({ error: 'invalid token' });
+	}
+
+	const user = await User.findById(decodedToken.id);
 
 	const blog = new Blog({
 		title,
