af9ef712e5c2d036fa5b2ee62ab0db1784b159f0
Arjun Sah <arjunsah@sussol.net>
2023-12-27 15:56:04 +0545

    applied populate method as join between tables

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const userRouter = require('express').Router();
 const User = require('../models/user');
 
 userRouter.get('/', async(req, res) => {
-	const users = await User.find({});
+	const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 });
 	res.json(users);
 });
 
