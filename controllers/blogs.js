dc9cf9a57f26b49e82a4a6543e3760d88c111a65
Arjun Sah <arjunsah@sussol.net>
2023-12-28 12:32:04 +0545

    extract token to request

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,15 +4,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = request => {
-	const authorization = request.get('authorization');
-	if (authorization && authorization.startsWith('Bearer ')) {
-		return authorization.replace('Bearer ', '');
-	}
-
-	return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
 	response.json(blogs);
@@ -20,22 +11,22 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (req, res, next) => {
 	const { title, url, likes, author } = req.body;
-	const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
-	if (!decodedToken.id) {
-		return res.status(401).json({ error: 'invalid token' });
-	}
-
-	const user = await User.findById(decodedToken.id);
-
-	const blog = new Blog({
-		title,
-		url,
-		likes,
-		author,
-		user: user.id
-	});
-
 	try {
+		const decodedToken = jwt.verify(req.token, process.env.SECRET);
+		if (!decodedToken.id) {
+			return res.status(401).json({ error: 'invalid token' });
+		}
+
+		const user = await User.findById(decodedToken.id);
+
+		const blog = new Blog({
+			title,
+			url,
+			likes,
+			author,
+			user: user.id
+		});
+
 		const savedBlog = await blog.save();
 		user.blogs = user.blogs.concat(savedBlog._id);
 		await user.save();
